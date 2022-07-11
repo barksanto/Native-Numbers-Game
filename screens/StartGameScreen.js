ef02973b36667c391904de7982c4f1d95b44d69f
Barkley <barkley.santo@gmail.com>
2022-07-11 16:57:53 +0300

    create different screen components, start primary button component

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,13 @@
+import { View, TextInput } from "react-native"
+import PrimaryButton from "../components/PrimaryButton"
+
+function StartGameScreen() {
+  return (
+    <View>
+      <TextInput placeholder="enter txt here" />
+      <PrimaryButton />
+    </View>
+  )
+}
+
+export default StartGameScreen
