c8c42f3f44837cbdfa01495d28d32455c9b78101
Barkley <barkley.santo@gmail.com>
2022-07-13 14:18:49 +0300

    shadows to both ios and android

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -18,6 +18,18 @@ const styles = StyleSheet.create({
     // flex: 1, // tells component to take up as much space available - commenting it out allows it to use only as much space as it needs
     padding: 16,
     marginTop: 100,
-    backgroundColor: "#27063c",
+    backgroundColor: "#72063c",
+    marginHorizontal: 24,
+    borderRadius: 6,
+
+    elevation: 10, //android shadow
+
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 6,
   },
 })
