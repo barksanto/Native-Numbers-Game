0b7243dddb7c4c45c4a6c9bc2c58c94f37645358
Barkley <barkley.santo@gmail.com>
2022-07-18 14:55:29 +0100

    add SafeAreaView component around our screen variable - so our top text area is sized properly and not blocked by the notches in smartphones

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, ImageBackground } from "react-native"
+import { StyleSheet, ImageBackground, SafeAreaView } from "react-native"
 import StartGameScreen from "./screens/StartGameScreen"
 import { LinearGradient } from "expo-linear-gradient"
 import { useState } from "react"
@@ -26,7 +26,7 @@ export default function App() {
         imageStyle={styles.backgroundImage}
       >
         {/* <StartGameScreen /> */}
-        {screen}
+        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
       </ImageBackground>
     </LinearGradient>
   )
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,30 @@
-import { Text } from "react-native"
+import { View, Text, StyleSheet } from "react-native"
 
 function GameScreen() {
-  return <Text>Gamescreen!</Text>
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.title}>Opponent's Guess!</Text>
+      {/* GUESS */}
+      <View>
+        <Text>Higher or Lower</Text>
+        {/* +
+        - */}
+      </View>
+      <Text>LOG ROUNDS</Text>
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#ddb52f",
+  },
+})
 
 export default GameScreen
